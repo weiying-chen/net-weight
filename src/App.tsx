ee77a8905abfe78a49ca4ca73b92ed2ffb6141db
Wei-ying Chen <weiying.alex.chen@gmail.com>
2024-07-29 20:49:43 +0800

    Refactor `Card`

--- src/App.tsx
+++ src/App.tsx
@@ -9,32 +9,7 @@ function App() {
   return (
     <div className="p-4 flex justify-center">
       {Object.keys(groupedData).map((item) => (
-        <div
-          key={item}
-          className="bg-white border-[#083355] border-[1.5px] rounded-3xl [box-shadow:4px_4px_0px_rgba(0,0,0,0.1)] w-full max-w-2xl mb-6"
-        >
-          <img
-            src="https://via.placeholder.com/600x300"
-            alt="Placeholder Image"
-            className="w-full h-auto rounded-t-3xl"
-          />
-          <div className="py-6">
-            <h2 className="font-semibold tracking-tight text-4xl text-center">
-              {item}
-            </h2>
-            {groupedData[item].length > 0 && (
-              <>
-                <Card reading={groupedData[item][0]} isLastReading />
-                {groupedData[item].slice(1).map((reading) => (
-                  <div key={reading.id}>
-                    <hr className="my-4 border-gray-300" />
-                    <Card reading={reading} />
-                  </div>
-                ))}
-              </>
-            )}
-          </div>
-        </div>
+        <Card key={item} item={item} readings={groupedData[item]} />
       ))}
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,64 @@
-import React from "react";
 import { Reading } from "../types";
 
-type CardProps = {
-  reading: Reading;
-  isLastReading?: boolean;
+const CardHeader: React.FC<{ item: string }> = ({ item }) => {
+  return (
+    <>
+      <img
+        src="https://via.placeholder.com/600x300"
+        alt="Placeholder Image"
+        className="w-full h-auto rounded-t-3xl"
+      />
+      <div className="py-6">
+        <h2 className="font-semibold tracking-tight text-4xl text-center">
+          {item}
+        </h2>
+      </div>
+    </>
+  );
 };
 
-const Card: React.FC<CardProps> = ({ reading, isLastReading }) => {
+const CardContent: React.FC<{ reading: Reading; isFirstReading?: boolean }> = ({
+  reading,
+  isFirstReading,
+}) => {
   return (
     <div className="px-4 space-y-2">
       <h3
         className={
-          isLastReading ? "text-2xl font-semibold" : "text-sm font-semibold"
+          isFirstReading ? "text-2xl font-semibold" : "text-sm font-semibold"
         }
       >
         Weight: {reading.weight.toFixed(2)} g
       </h3>
-      <p className={`text-gray-400 ${isLastReading ? "text-md" : "text-xs"}`}>
+      <p className={`text-gray-400 ${isFirstReading ? "text-md" : "text-xs"}`}>
         {new Date(reading.createdAt).toLocaleString()}
       </p>
+    </div>
+  );
+};
+
+type CardProps = {
+  item: string;
+  readings: Reading[];
+};
+
+const Card: React.FC<CardProps> = ({ item, readings }) => {
+  return (
+    <div className="bg-white border-[#083355] border-[1.5px] rounded-3xl [box-shadow:4px_4px_0px_rgba(0,0,0,0.1)] w-full max-w-2xl mb-6">
+      <CardHeader item={item} />
+      <div>
+        {readings.length > 0 && (
+          <>
+            <CardContent reading={readings[0]} isFirstReading />
+            {readings.slice(1).map((reading) => (
+              <div key={reading.id}>
+                <hr className="my-4 border-gray-300" />
+                <CardContent reading={reading} />
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
