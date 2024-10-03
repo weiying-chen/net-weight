2268f51afaa4324ff1381bb6f4fe32ab63fb6cdb
Wei-ying Chen <weiying.alex.chen@gmail.com>
2024-10-03 17:09:45 +0800

    Refactor file previews

--- src/components/FilePreviews.tsx
+++ src/components/FilePreviews.tsx
@@ -14,6 +14,32 @@ type FilePreviewsProps = {
 };
 
 export function FilePreviews({ files, removeFile }: FilePreviewsProps) {
+  const renderFilename = (file: FileData) => (
+    <Row
+      align="center"
+      alignItems="center"
+      className="absolute bottom-0 flex h-7 overflow-hidden border-t border-border bg-background p-2 text-xs"
+      locked
+    >
+      <span className="truncate text-ellipsis whitespace-nowrap">
+        {file.name}
+      </span>
+    </Row>
+  );
+
+  const renderX = (index: number) => {
+    if (!removeFile) return null;
+
+    return (
+      <button
+        className="absolute right-1 top-1 z-10 rounded-full bg-foreground p-1 text-background opacity-0 shadow transition-opacity duration-200 hover:shadow-dark group-hover:opacity-100"
+        onClick={() => removeFile(index)}
+      >
+        <IconX size={16} />
+      </button>
+    );
+  };
+
   return (
     <div className="grid grid-cols-4 gap-2 md:grid-cols-8">
       {files.map((file, index) => (
@@ -21,14 +47,7 @@ export function FilePreviews({ files, removeFile }: FilePreviewsProps) {
           key={index}
           className="aspect-w-1 aspect-h-1 group relative w-full overflow-hidden rounded border border-border pb-7"
         >
-          {removeFile && (
-            <button
-              className="absolute right-1 top-1 z-10 rounded-full bg-foreground p-1 text-background opacity-0 shadow transition-opacity duration-200 hover:shadow-dark group-hover:opacity-100"
-              onClick={() => removeFile(index)}
-            >
-              <IconX size={16} />
-            </button>
-          )}
+          {renderX(index)}
           {file.url ? (
             <img
               src={file.url}
@@ -40,16 +59,7 @@ export function FilePreviews({ files, removeFile }: FilePreviewsProps) {
               <IconFileDescription size={60} stroke={1} />
             </Col>
           )}
-          <Row
-            align="center"
-            alignItems="center"
-            className="absolute bottom-0 flex h-7 overflow-hidden border-t border-border bg-background p-2 text-xs"
-            locked
-          >
-            <span className="truncate text-ellipsis whitespace-nowrap">
-              {file.name}
-            </span>
-          </Row>
+          {renderFilename(file)}
         </div>
       ))}
     </div>
